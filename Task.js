9fcaea2481f5d5448efb9c213feedf2976cc9a90
Mohanasakthi S <mohanasakthiselvaraj6@gmail.com>
2026-08-25 18:46:41 +0530

    Add files via upload

---
+++ Task.js
@@ -0,0 +1,24 @@
+1.
+var studentName = "Mohanasakthi";
+let studentAge = 22;
+const studentMark = 80;
+
+console.log(studentName);
+console.log(studentAge);
+console.log(studentMark);
+
+2.
+var userName = prompt("Enter your name :")
+alert(userName);
+
+3.
+var userAge = prompt("Enter user age :")
+console.log(userAge);
+
+4.
+var userMark = confirm("Did you filled your details ?")
+
+
+5. 
+var userName = prompt("User name :");
+document.writeln(userName);
